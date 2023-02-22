6f11d7478d1d90d68103c1aa02fa4d9e0926de58
Jia <71865277+jangjia01234@users.noreply.github.com>
2023-02-22 20:48:46 +0900

    :heavy_plus_sign: 초기 세팅 완료

--- frontend/src/pages/Login/Login.js
+++ frontend/src/pages/Login/Login.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 // import { useEffect } from "react";
 import "../../styles/reset.scss";
@@ -6,42 +6,28 @@ import "../../styles/common.scss";
 import "../Login/Login.scss";
 
 const Login = () => {
+  const [id, setId] = useState("");
+  const [pw, setPw] = useState("");
+  const [color, setColor] = useState("#c5e1fa");
+
+  const saveUserId = (e) => {
+    setId(e.target.value);
+    // if (e.targat.alue.length >= 5) alert("성공");
+  };
+  const saveUserPw = (e) => {
+    setPw(e.target.value);
+  };
+
+  // 키 이벤트 감지 -> 버튼 활성화
+  const toggleBtnColor = () => {
+    id && pw ? setColor("#5395e9") : setColor("#c5e1fa");
+  };
+
   // const navigate = useNavigate();
 
   // const goToMain = () => {
   //   navigate("/main");
   // };
-
-  // useEffect(() => {
-  //   let loginIdInput = document.querySelector(".id-input");
-  //   let loginPwInput = document.querySelector(".pw-input");
-  //   let loginBtn = document.querySelector(".login-btn");
-
-  // 키 이벤트 감지 -> 버튼 활성화
-
-  // function checkInput() {
-  //   if (loginIdInput.value && loginPwInput.value) {
-  //     loginBtn.style.backgroundColor = "#5395e9";
-  //     loginBtn.style.cursor = "pointer";
-  //     loginBtn.disabled = false;
-  //   } else {
-  //     loginBtn.style.backgroundColor = "#c5e1fa";
-  //     loginBtn.disabled = true;
-  //   }
-  // }
-
-  // ID, PW validation
-
-  //   function checkLetter() {
-  //     loginIdInput.value.includes("@") && loginPwInput.value.length >= 5
-  //       ? navigate("/main")
-  //       : alert("아이디 혹은 패스워드를 확인해주세요.");
-  //   }
-
-  //   loginIdInput.addEventListener("keydown", checkInput);
-  //   loginPwInput.addEventListener("keydown", checkInput);
-  //   loginBtn.addEventListener("click", checkLetter);
-  // });
 
   return (
     <div className="Login">
@@ -54,18 +40,28 @@ const Login = () => {
                 className="id-input"
                 type="text"
                 placeholder="전화번호, 사용자 이름 또는 이메일"
+                onChange={saveUserId}
+                value={id}
               />
               <input
                 className="pw-input"
                 type="password"
                 placeholder="비밀번호"
+                onChange={saveUserPw}
+                value={pw}
               />
             </div>
-            <Link to="/main">
-              <div className="login-btn-box">
-                <button className="login-btn">로그인</button>
-              </div>
-            </Link>
+            {/* <Link to="/main"> */}
+            <div className="login-btn-box">
+              <button
+                className="login-btn"
+                onClick={toggleBtnColor} // 자동으로 바뀌게 수정 필요
+                style={{ backgroundColor: color }}
+              >
+                로그인
+              </button>
+            </div>
+            {/* </Link> */}
             {/* <button className="login-btn-box" onClick={goToMain}>
               로그인
             </button> */}
@@ -80,3 +76,20 @@ const Login = () => {
 };
 
 export default Login;
+
+// useEffect(() => {
+//   let loginIdInput = document.querySelector(".id-input");
+//   let loginPwInput = document.querySelector(".pw-input");
+//   let loginBtn = document.querySelector(".login-btn");
+
+// ID, PW validation
+//   function checkLetter() {
+//     loginIdInput.value.includes("@") && loginPwInput.value.length >= 5
+//       ? navigate("/main")
+//       : alert("아이디 혹은 패스워드를 확인해주세요.");
+//   }
+
+//   loginIdInput.addEventListener("keydown", checkInput);
+//   loginPwInput.addEventListener("keydown", checkInput);
+//   loginBtn.addEventListener("click", checkLetter);
+// });
